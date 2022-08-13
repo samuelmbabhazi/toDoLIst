ea332fbdf3a4f97d548432cdc1c1bd0871aa908e
samuelmbabhazi <samuelmbabhazi42@gmail.com>
2022-08-13 19:46:15 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let input = document.querySelector("#entre-list");
+let add = document.querySelector(".add-list");
+let tache = document.querySelector(".taches");
+
+add.addEventListener("click", () => {
+  if (input.value.trim() != 0) {
+    let newItem = document.createElement("div");
+    newItem.classList.add("item");
+    newItem.innerHTML = `<p style="outline: none" contenteditable><input type="checkbox" class="coche"> ${input.value}<p><div><img class="trash" src="./trash-can-solid.svg" alt="" width="15px"></div>`;
+    tache.appendChild(newItem);
+    input.value = "";
+  } else {
+    alert("Ajouter une tache");
+  }
+});
+tache.addEventListener("click", (e) => {
+  if (e.target.classList.contains("coche")) {
+    e.target.parentElement.parentElement.classList.toggle("completed");
+  }
+});
+
+tache.addEventListener("click", (e) => {
+  if (e.target.classList.contains("trash")) {
+    e.target.parentElement.parentElement.remove();
+  }
+});
